43d8db55a6e5e0db9bf579bbeb9b45ae942212a2
liuwenzhe <liuwenzhe_neoye@163.com>
2019-11-15 11:34:50 +0800

    chore: export func 'client' instead of 'newClient'

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Client from './client';
 import { IOptions, ISSEOptions, ClientError } from './IClient';
 
-export function newClient(url: string, events: string[], options?: IOptions) {
+export function client(url: string, events: string[], options?: IOptions) {
   return new Client(url, events, options);
 }
 export { IOptions, ISSEOptions, ClientError };
